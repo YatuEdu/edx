4a178cfb74f04b87d293eb5c2b99fcb7d915a59a
goofy <goofyluo@gmail.com>
2023-11-13 18:09:34 +0800

    屏幕录制demo更新，目前还有问题，音频只有一个

--- SRC/src/yatuedx/root/assets/js/core/videoUtil.js
+++ SRC/src/yatuedx/root/assets/js/core/videoUtil.js
@@ -1,3 +1,4 @@
+import Crunker from "../crunker/crunker.esm.js";
 
 export class VideoUtil {
 
@@ -25,5 +26,41 @@ export class VideoUtil {
         let mediaStream = await navigator.mediaDevices.getDisplayMedia({video: true});
         let videoTrack = mediaStream.getVideoTracks()[0];
         return videoTrack;
+    }
+
+    static async merge() {
+        const crunker = new Crunker();
+        return crunker
+            .fetchAudio('/assets/music/The_Skaters_Waltz_Rieu.mp3', '/assets/music/Legends_of_the_Fall_James_Horner.mp3')
+            .then((buffers) => {
+                // => [AudioBuffer, AudioBuffer]
+                return crunker.mergeAudio(buffers);
+            })
+            .then((merged) => {
+                // => AudioBuffer
+                return crunker.export(merged, 'audio/mp3');
+            })
+    }
+
+    static async convertAudioTrackToAudioBuffer(audioTrack) {
+        const audioContext = new AudioContext();
+        const audioBuffer = await new Promise((resolve, reject) => {
+            const reader = new FileReader();
+            reader.onload = function () {
+                const audioData = reader.result;
+                console.log('audioData.length: ', audioData.byteLength);
+                audioContext.decodeAudioData(audioData, resolve, reject);
+            };
+            reader.onerror = reject;
+            const blob = new Blob([audioTrack]);
+            reader.readAsArrayBuffer(blob);
+        });
+        return audioBuffer;
+    }
+
+    static async merge(audioTrack1, audioTrack2) {
+        const crunker = new Crunker();
+        let buffers = [await this.convertAudioTrackToAudioBuffer(audioTrack1), await this.convertAudioTrackToAudioBuffer(audioTrack2)];
+        return crunker.mergeAudio(buffers);
     }
 }
--- SRC/src/yatuedx/root/assets/js/test/ScreenRecordTest.js
+++ SRC/src/yatuedx/root/assets/js/test/ScreenRecordTest.js
@@ -1,7 +1,33 @@
 import {VideoUtil} from "../core/videoUtil.js";
 
 let myVideoTrack = null;
-let myAudioTrack = null;
+let myAudioTrack1 = null;
+let myAudioTrack2 = null;
+
+async function audioFile2Track(filePath) {
+    const audioContext = new AudioContext();
+    const response = await fetch(filePath);
+    const audioData = await response.arrayBuffer();
+    const audioBuffer = await audioContext.decodeAudioData(audioData);
+    const source = audioContext.createBufferSource();
+    source.buffer = audioBuffer;
+    let dest = audioContext.createMediaStreamDestination();
+    source.connect(dest);
+    source.start();
+    let stream = dest.stream;
+    let audioTrack = stream.getAudioTracks()[0];
+    return audioTrack;
+}
+
+function printAudioTrackInfo(audioTrack) {
+    console.log('Audio Track Details:');
+    console.log('ID: ' + audioTrack.id);
+    console.log('Label: ' + audioTrack.label);
+    console.log('Kind: ' + audioTrack.kind);
+    console.log('Muted: ' + audioTrack.muted);
+    console.log('Enabled: ' + audioTrack.enabled);
+    console.log('Ready State: ' + audioTrack.readyState);
+}
 
 $(document).ready(async function () {
     console.log("ScreenRecord page ready!");
@@ -11,22 +37,29 @@ $(document).ready(async function () {
     $("#startBtn").click(async e => {
         $("#startBtn").hide();
         $("#stopBtn").show();
-
+        //await VideoUtil.merge();
         let tracks;
         try {
             myVideoTrack = await VideoUtil.getScreenShareTrack();
-            tracks = await VideoUtil.getLocalMediaTracks(true, false);
+            // tracks = await VideoUtil.getLocalMediaTracks(true, true);
+            // myAudioTrack = tracks[0];
+            // printAudioTrackInfo(myAudioTrack);
+            let audioFile1 = "/assets/music/The_Skaters_Waltz_Rieu.mp3";
+            myAudioTrack1 = await audioFile2Track(audioFile1);
+            let audioFile2 = "/assets/music/Legends_of_the_Fall_James_Horner.mp3";
+            myAudioTrack2 = await audioFile2Track(audioFile1);
         } catch (e) {
             console.error(e);
             return;
         }
-        myAudioTrack = tracks[0];
+        //myAudioTrack = tracks[0];
 
         // 下面是录制的过程
         // 录制的对象是 mediaStream，需要创建一个，并把需要的音频轨或视频轨加进去
         const mediaStream = new MediaStream();
         // 将音频轨道添加到 MediaStream
-        mediaStream.addTrack(myAudioTrack);
+        mediaStream.addTrack(myAudioTrack1);
+        mediaStream.addTrack(myAudioTrack2);
         // 将视频轨道添加到 MediaStream
         mediaStream.addTrack(myVideoTrack);
 
